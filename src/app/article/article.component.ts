4fd20c370d3de11b83e84fd4e713dc4079dd27a2
edwinperezrojas <2000892@unmsm.edu.pe>
2021-11-03 16:59:39 -0500

    Se agrega el proyecto

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,34 @@
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article: Article;
+
+  constructor() {
+    this.article = new Article(
+      'Angular 2',
+      'http://angular.io',
+      10);
+  }
+
+  voteUp(): boolean  {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean  {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
